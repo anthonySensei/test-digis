78eef7d65a24d85c280ed11f82cd10d75e844854
Anton Shevchuk <w.s.f.antonshevchuk@gmail.com>
2021-02-06 18:53:17 +0200

    changed configuration pf eslint, deleted web vitals, installed dependencies for redux, added and configured redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import rootReducer from './store/reducers';
+
+import './index.css';
+
+const composeEnhancers =
+  (process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null) || compose;
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
